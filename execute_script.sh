a29681815bd6bafcabc611229076c9608b79dee0
Biswas Biplob <Biplob.Biswas@burda-forward.de>
2021-05-18 23:24:21 +0200

    Update code to use Microsoft authenticator

---
+++ execute_script.sh
@@ -0,0 +1,7 @@
+pyenv version
+cd /Users/d441303/Documents/projects/personal/Microsoft-Rewards-Farmer
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+pyenv activate msrewards
+pyenv version
+python ms_rewards_farmer.py
